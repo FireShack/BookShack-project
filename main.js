6c38898b54fd8437801f0ba54eda692d6c1d801e
Franco <francoguardini10@gmail.com>
2021-11-06 14:51:45 +0100

    some new changes

--- main.js
+++ main.js
@@ -4,8 +4,11 @@ const navbarCollapse = document.querySelector('.navbar-collapse')
 const navbarBrand = document.querySelector('.navbar-brand')
 const dropdownMenu = document.querySelector('.dropdown-menu')
 const navbar = document.querySelector('.navbar')
+const btn_member = document.querySelector('.btn-member')
+const titulo_header = document.querySelector('.titulo-header')
 
 window.addEventListener('scroll', scroll)
+window.addEventListener('scroll', btn_on)
 
 
 
@@ -17,8 +20,22 @@ function scroll() {
         navbar.classList.add('nav-oscuro')
         dropdownMenu.classList.add('nav-oscuro')
         navbarCollapse.classList.remove('nav-transp')
+
     } else {
         navbar.classList.remove('nav-oscuro')
         navbarCollapse.classList.add('nav-transp')
+
+    }
+}
+
+function btn_on() {
+    const ubicacion = iconos.getBoundingClientRect()
+
+    if (ubicacion.top >= 450) {
+        btn_member.classList.add('btn-member-inhabilita')
+        btn_member.classList.add('btn-animacion')
+
+    } else {
+        btn_member.classList.remove('btn-member-inhabilita')
     }
 }
